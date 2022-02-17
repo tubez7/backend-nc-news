7ae7cb3122e6f6ef22bd8c4e26a704a2bf38235c
tubez7 <96090739+tubez7@users.noreply.github.com>
2022-02-17 13:17:51 +0000

    Update users-models.js

--- models/users-models.js
+++ models/users-models.js
@@ -2,6 +2,7 @@ const db = require("../db/connection.js");
 
 exports.fetchUsers = () => {
     return db.query("SELECT username FROM users;").then((users) => {
+        // result renamed users. users is promise that resolves as object returned from db
         return users.rows;
     })
     
