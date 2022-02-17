9089df194731f1621dd719ebcd9d64a1fbf09058
tubez7 <96090739+tubez7@users.noreply.github.com>
2022-02-17 11:39:31 +0000

    Update users-models.js

--- models/users-models.js
+++ models/users-models.js
@@ -2,8 +2,6 @@ const db = require("../db/connection.js");
 
 exports.fetchUsers = () => {
     return db.query("SELECT username FROM users;").then((users) => {
-        console.log(users);
-        console.log(users.rows);
         return users.rows;
     })
     
