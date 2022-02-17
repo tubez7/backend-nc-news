105b4fc8e8f9c2692a6da3022c5e668e5935f4ad
tubez7 <96090739+tubez7@users.noreply.github.com>
2022-02-17 11:37:59 +0000

    Update users-controllers.js

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -3,7 +3,6 @@ const {fetchUsers} = require("../models/users-models.js");
 exports.getUsers = (req, res, next) => {
   fetchUsers()
     .then((users) => {
-      console.log(users); //returned promise from model. Resolves to array of objects.
       res.status(200).send({ users });
     })
     .catch((err) => {
