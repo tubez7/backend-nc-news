ce4512128cd9b67d0ac305fedcfcc48989287e38
tubez7 <thescopeboys@hotmail.com>
2022-03-06 14:23:59 +0000

    complete ncnews-10

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -71,6 +71,23 @@ describe("GET request on /api/articles", () => {
   });
 });
 
+describe("REFACTORED GET request on /api/articles", () => {
+  test("should respond with status 200 and an array of all article objects, including a comment_count property, on a key of articles", () => {
+    return request(app)
+      .get("/api/articles")
+      .expect(200)
+      .then(({ body }) => {
+        body.articles.forEach((article) => {
+          expect(article).toEqual(
+            expect.objectContaining({
+              comment_count: expect.any(Number),
+            })
+          );
+        });
+      });
+  });
+});
+
 describe("GET request on /api/articles/:article_id", () => {
   test("should respond with status 200 and a single article object on a key of article", () => {
     const articleId = 1;
@@ -336,7 +353,6 @@ describe("Errors on POST /api/articles/:article_id/comments", () => {
       });
   });
 });
-
 
 //  GET REQUEST FOR COMMENTS BY ARTICLE ID
 
